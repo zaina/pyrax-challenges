e19925afceb6c2c6f7c840b79f37bacc14b2506c
Zaina <zenetcontact@gmail.com>
2013-03-16 12:33:08 -0500

    Challenge1

---
+++ challenge1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import pyrax
+import os
+import time
+
+print "Using credentials file"
+credentials_file = os.path.expanduser("~/.rackspace_cloud_credentials")
+try:
+	pyrax.set_credential_file(credentials_file)
+except pyrax.exceptions.AuthenticationFailed:
+	print "Anthentication failed. Valid credentials needed in ~/.rackspace_cloud_credentials"
+print "authenticated =", pyrax.identity.authenticated
+print
+
+cs = pyrax.cloudservers
+flavor_512 = [flavor for flavor in cs.flavors.list() if flavor.ram == 512][0]
+cent_img = [img for img in cs.images.list() if "CentOS" in img.name][0]
+
+server_names = ["zaina1","zaina2","zaina3"]
+for name in server_names:
+	print "Creating server %s..." %name
+	server = cs.servers.create(name, cent_img.id, flavor_512.id)
+	print "Root Password: %s" % server.adminPass
+	while not cs.servers.get(server.id).networks:
+		time.sleep(1)
+	network = cs.servers.get(server.id).networks
+	print "Public IP Address: %s" % network["public"]
+	print "Private IP Address: %s" % network["private"]
+	print
+
